5331620c2dfb4150f179b893a84b633e0f123965
Kevin Tun <kevintun01@gmail.com>
2020-02-24 14:23:11 -0600

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    mode: 'development',
+    entry: [
+      '@babel/polyfill', // enables async-await
+      './client/index.js'
+    ],
+    output: {
+      path: __dirname,
+      filename: './public/bundle.js'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        }
+      ]
+    }
+  }
